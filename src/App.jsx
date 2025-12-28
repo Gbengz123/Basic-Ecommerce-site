3eab86bc23accd81a27df3f9d424599745e7b562
Danny <danny@Daniels-MacBook-Air.local>
2025-12-28 20:16:39 +0100

    Install and setup theme with daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="d-btn bg-neutral text-neutral-content hover:bg-neutral-light rounded-2xl border-0">
+        Hello
+      </div>
+    </>
+  );
 }
 
 export default App;
