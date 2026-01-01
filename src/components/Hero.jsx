7ee59441ba53a8612f2b4878f89167095dc4503b
Danny <danny@Daniels-MacBook-Air.local>
2026-01-01 22:18:11 +0100

    Edit styling and add images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router';
+import jacket from '../assets/images/jacket.jpg';
+import bracelet from '../assets/images/bracelet.jpg';
+import womenJacket from '../assets/images/womenJacket.jpg';
 
 function Hero() {
+  const cardStyle =
+    'bg-cover bg-center shadow-2xl hover:-translate-y-[10px] transition-transform ease-in duration-200 hover:cursor-pointer';
   return (
-    <section className="page-padding flex flex-col items-center py-8 sm:flex-row">
-      <div className="z-20 flex flex-col gap-2">
-        <h1 className="text-5xl font-bold">Quality products, delivered fast</h1>
+    <section className="page-padding flex flex-col items-center gap-4 py-8 sm:flex-row sm:gap-0">
+      <div className="z-20 flex flex-col items-center gap-4 sm:items-start sm:gap-2">
+        <h1 className="w-[80%] text-center text-5xl font-bold sm:w-full sm:text-left">
+          Quality products, delivered fast
+        </h1>
         <p className="text-lg text-gray-600">
           Everything you need, in one place.
         </p>
@@ -17,9 +24,24 @@ function Hero() {
         </Link>
       </div>
       <div className="xs:grid-cols-[repeat(7,calc(180px/4))] xs:grid-rows-[190px] xs:mt-4 mt-4 grid grow grid-cols-[repeat(7,calc(140px/4))] grid-rows-[150px] justify-center sm:mt-0 sm:mr-11 md:grid-cols-[repeat(7,calc(200px/4))] md:grid-rows-[210px]">
-        <div className="col-start-1 col-end-4 row-start-1 origin-bottom -rotate-10 skew-x-3 rounded-lg border bg-blue-800"></div>
-        <div className="z-10 col-start-3 col-end-6 row-start-1 rounded-lg border bg-black"></div>
-        <div className="left-70 col-start-5 col-end-8 row-start-1 origin-bottom rotate-10 -skew-x-3 rounded-lg border"></div>
+        <div
+          style={{
+            backgroundImage: `url(${womenJacket})`,
+          }}
+          className={`col-start-1 col-end-4 row-start-1 origin-bottom -rotate-10 skew-x-3 rounded-lg ${cardStyle}`}
+        ></div>
+        <div
+          style={{
+            backgroundImage: `url(${bracelet})`,
+          }}
+          className={`z-10 col-start-3 col-end-6 row-start-1 rounded-lg ${cardStyle}`}
+        ></div>
+        <div
+          style={{
+            backgroundImage: `url(${jacket})`,
+          }}
+          className={`col-start-5 col-end-8 row-start-1 origin-bottom rotate-10 -skew-x-3 rounded-lg border ${cardStyle}`}
+        ></div>
       </div>
     </section>
   );
