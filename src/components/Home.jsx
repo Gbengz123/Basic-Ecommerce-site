d25993736f106e746a6e7e25cba11985021feea8
Danny <danny@Daniels-MacBook-Air.local>
2026-01-05 19:23:38 +0100

    improve accessibility of bottom section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import mensFashion from '../assets/images/mensFashion.jpg';
 import womensFashion from '../assets/images/womens.jpg';
 import jewelry from '../assets/images/jewelry.jpg';
 import electronics from '../assets/images/electronics.jpg';
+import { Link } from 'react-router';
 
 function Home() {
   const bgImage = (src) =>
@@ -18,9 +19,11 @@ function Home() {
         <ProductsCarousel />
       </section>
       <section className="page-padding flex grow flex-col gap-2 py-8">
-        <h2 className="text-xl font-bold">Categories</h2>
+        <h2 id="categories" className="text-xl font-bold">
+          Categories
+        </h2>
         <div className="grid grow grid-cols-[repeat(auto-fit,minmax(230px,1fr))] gap-2.5 md:grid-cols-[minmax(240px,1fr)_minmax(200px,0.8fr)_minmax(220px,0.9fr)] md:grid-rows-[repeat(2,minmax(180px,1fr))]">
-          <div
+          <Link
             style={{
               backgroundImage: bgImage(mensFashion),
             }}
@@ -29,37 +32,37 @@ function Home() {
             <span className="absolute top-4 left-8 transition-transform duration-100 ease-linear group-hover:translate-x-1.5">
               Men's Clothing
             </span>
-          </div>
-          <div
+          </Link>
+          <Link
             style={{
               backgroundImage: bgImage(jewelry),
             }}
-            className={`h-70 bg-red-800 md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-2 md:h-auto ${categoryStyle}`}
+            className={`h-70 md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-2 md:h-auto ${categoryStyle}`}
           >
             <span className="absolute top-4 left-8 transition-transform duration-100 ease-linear group-hover:translate-x-1.5">
               Jewelry
             </span>
-          </div>
-          <div
+          </Link>
+          <Link
             style={{
               backgroundImage: bgImage(electronics),
             }}
-            className={`h-70 bg-blue-800 md:col-start-2 md:col-end-3 md:row-start-2 md:row-end-3 md:h-auto ${categoryStyle}`}
+            className={`h-70 md:col-start-2 md:col-end-3 md:row-start-2 md:row-end-3 md:h-auto ${categoryStyle}`}
           >
             <span className="absolute top-4 left-8 transition-transform duration-100 ease-linear group-hover:translate-x-1.5">
               Electronics
             </span>
-          </div>
-          <div
+          </Link>
+          <Link
             style={{
               backgroundImage: bgImage(womensFashion),
             }}
-            className={`h-70 bg-green-900 md:col-start-3 md:col-end-4 md:row-start-1 md:row-end-3 md:h-auto ${categoryStyle}`}
+            className={`h-70 md:col-start-3 md:col-end-4 md:row-start-1 md:row-end-3 md:h-auto ${categoryStyle}`}
           >
             <span className="absolute top-4 left-8 transition-transform duration-100 ease-linear group-hover:translate-x-1.5">
               Womens Clothing
             </span>
-          </div>
+          </Link>
         </div>
       </section>
     </div>
