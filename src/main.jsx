26c90c5e4e633fd938192fd8232b330ac6973194
Danny <danny@Daniels-MacBook-Air.local>
2026-01-05 19:32:24 +0100

    Create routing instace

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { RouterProvider } from 'react-router';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 import routes from './routes.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={routes} />
+    <RouterProvider router={createBrowserRouter(routes)} />
   </StrictMode>,
 );
