1e87d7d346524b8c1f90f66ce0fc7b0c4ba27708
Danny <danny@Daniels-MacBook-Air.local>
2026-01-06 19:28:24 +0100

    Add skeleton

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,50 +3,59 @@ import { Link } from 'react-router';
 import jacket from '../assets/images/jacket.jpg';
 import bracelet from '../assets/images/bracelet.jpg';
 import womenJacket from '../assets/images/womenJacket.jpg';
+import { Skeleton } from './ui/skeleton';
 
-function Hero() {
+function Hero({ showSkeleton }) {
   const cardStyle =
     'bg-cover bg-center shadow-2xl hover:-translate-y-[10px] transition-transform ease-in duration-200 hover:cursor-pointer';
   return (
-    <section className="page-padding flex flex-col items-center gap-4 py-8 sm:flex-row sm:gap-0">
-      <div className="z-20 flex flex-col items-center gap-4 sm:items-start sm:gap-2">
-        <h1 className="w-[80%] text-center text-5xl font-bold sm:w-full sm:text-left">
-          Quality products, delivered fast
-        </h1>
-        <p className="text-lg text-gray-600">
-          Everything you need, in one place.
-        </p>
-        <Link
-          to={'/shop'}
-          className="d-btn bg-neutral text-neutral-content w-32 rounded-lg"
-        >
-          Shop now
-        </Link>
-      </div>
-      <div
-        data-testid="card display"
-        className="xs:grid-cols-[repeat(7,calc(180px/4))] xs:grid-rows-[190px] xs:mt-4 mt-4 grid grow grid-cols-[repeat(7,calc(140px/4))] grid-rows-[150px] justify-center sm:mt-0 sm:mr-11 md:grid-cols-[repeat(7,calc(200px/4))] md:grid-rows-[210px]"
-      >
-        <div
-          style={{
-            backgroundImage: `url(${womenJacket})`,
-          }}
-          className={`col-start-1 col-end-4 row-start-1 origin-bottom -rotate-10 skew-x-3 rounded-lg ${cardStyle}`}
-        ></div>
-        <div
-          style={{
-            backgroundImage: `url(${bracelet})`,
-          }}
-          className={`z-10 col-start-3 col-end-6 row-start-1 rounded-lg ${cardStyle}`}
-        ></div>
-        <div
-          style={{
-            backgroundImage: `url(${jacket})`,
-          }}
-          className={`col-start-5 col-end-8 row-start-1 origin-bottom rotate-10 -skew-x-3 rounded-lg border ${cardStyle}`}
-        ></div>
-      </div>
-    </section>
+    <>
+      {!showSkeleton ? (
+        <section className="page-padding flex flex-col items-center gap-4 py-8 sm:flex-row sm:gap-0">
+          <div className="z-20 flex flex-col items-center gap-4 sm:items-start sm:gap-2">
+            <h1 className="w-[80%] text-center text-5xl font-bold sm:w-full sm:text-left">
+              Quality products, delivered fast
+            </h1>
+            <p className="text-lg text-gray-600">
+              Everything you need, in one place.
+            </p>
+            <Link
+              to={'/shop'}
+              className="d-btn bg-neutral text-neutral-content w-32 rounded-lg"
+            >
+              Shop now
+            </Link>
+          </div>
+          <div
+            data-testid="card display"
+            className="xs:grid-cols-[repeat(7,calc(180px/4))] xs:grid-rows-[190px] xs:mt-4 mt-4 grid grow grid-cols-[repeat(7,calc(140px/4))] grid-rows-[150px] justify-center sm:mt-0 sm:mr-11 md:grid-cols-[repeat(7,calc(200px/4))] md:grid-rows-[210px]"
+          >
+            <div
+              style={{
+                backgroundImage: `url(${womenJacket})`,
+              }}
+              className={`col-start-1 col-end-4 row-start-1 origin-bottom -rotate-10 skew-x-3 rounded-lg ${cardStyle}`}
+            ></div>
+            <div
+              style={{
+                backgroundImage: `url(${bracelet})`,
+              }}
+              className={`z-10 col-start-3 col-end-6 row-start-1 rounded-lg ${cardStyle}`}
+            ></div>
+            <div
+              style={{
+                backgroundImage: `url(${jacket})`,
+              }}
+              className={`col-start-5 col-end-8 row-start-1 origin-bottom rotate-10 -skew-x-3 rounded-lg border ${cardStyle}`}
+            ></div>
+          </div>
+        </section>
+      ) : (
+        <div className="page-padding h-80 w-full">
+          <Skeleton className="page-padding mt-4 h-full w-full" />
+        </div>
+      )}
+    </>
   );
 }
 
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,32 +2,54 @@ import React, { useState } from 'react';
 import { ShoppingCartIcon } from 'lucide-react';
 import StarRating from './StarRating';
 import StepperInput from './StepperInput';
+import { Skeleton } from './ui/skeleton';
 
-function ProductCard({ category, rating, name, imgSrc, price }) {
+function ProductCardSkeleton() {
+  return (
+    <div className="border-base-200 flex w-56 flex-col gap-2 border px-3 py-1.5">
+      <Skeleton className="h-45 w-full" />
+      <Skeleton className="h-5 w-25" />
+      <Skeleton className="h-5 w-full" />
+      <Skeleton className="h-5 w-full" />
+    </div>
+  );
+}
+
+function ProductCard({ category, rating, name, imgSrc, price, showSkeleton }) {
   const [quantity, setQuantity] = useState(0);
 
   return (
-    <article className="flex w-56 cursor-pointer flex-col border border-zinc-200 px-3 py-1.5 text-sm outline-blue-700 transition-transform duration-100 ease-linear hover:scale-[1.01] hover:shadow sm:text-base">
-      <img src={imgSrc} alt="product image" className="h-45 w-full border" />
-      <h4 className="text-xs text-zinc-600">{category}</h4>
-      <StarRating rating={rating} />
-      <h3 className="py-2 font-semibold">{name}</h3>
-      <div className="flex items-center">
-        <p className="font-semibold">${price}</p>
-        {quantity >= 1 ? (
-          <StepperInput />
-        ) : (
-          <button
-            aria-label="add to cart"
-            className="d-btn bg-neutral text-neutral-content ml-auto flex h-fit items-center gap-1 border-none px-2 py-1"
-            onClick={() => setQuantity((quantity) => quantity + 1)}
-          >
-            <span>Add</span>
-            <ShoppingCartIcon height={20} />
-          </button>
-        )}
-      </div>
-    </article>
+    <>
+      {!showSkeleton ? (
+        <article className="flex w-56 cursor-pointer flex-col border border-zinc-200 px-3 py-1.5 text-sm transition-transform duration-100 ease-linear hover:scale-[1.01] hover:shadow sm:text-base">
+          <img
+            src={imgSrc}
+            alt="product image"
+            className="h-45 w-full border"
+          />
+          <h4 className="text-xs text-zinc-600">{category}</h4>
+          <StarRating rating={rating} />
+          <h3 className="py-2 font-semibold">{name}</h3>
+          <div className="flex items-center">
+            <p className="font-semibold">${price}</p>
+            {quantity >= 1 ? (
+              <StepperInput />
+            ) : (
+              <button
+                aria-label="add to cart"
+                className="d-btn bg-neutral text-neutral-content ml-auto flex h-fit items-center gap-1 border-none px-2 py-1"
+                onClick={() => setQuantity((quantity) => quantity + 1)}
+              >
+                <span>Add</span>
+                <ShoppingCartIcon height={20} />
+              </button>
+            )}
+          </div>
+        </article>
+      ) : (
+        <ProductCardSkeleton />
+      )}
+    </>
   );
 }
 
