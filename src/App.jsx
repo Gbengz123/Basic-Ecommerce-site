77ed0e5a3b6d0f09cd72243c47ed6fb5195df822
Danny <danny@Daniels-MacBook-Air.local>
2026-01-06 19:36:46 +0100

    Fetch shop data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 import './App.css';
-import { Outlet } from 'react-router';
+import { Navigate, Outlet } from 'react-router';
 import NavBar from './components/NavBar';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [shopData, setShopData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getShopData = async () => {
+      try {
+        const res = await fetch('https://fakestoreapi.com/products');
+        if (!res.ok) throw new Error(`HTTP Error: ${res.status}`);
+
+        const products = await res.json();
+        setShopData(products);
+      } catch (error) {
+        console.log(error);
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getShopData();
+  }, []);
+
+  console.log(shopData);
   return (
     <>
       <div className="flex min-h-screen min-w-screen flex-col">
         <NavBar />
         <main className="flex grow flex-col">
-          <Outlet />
+          <Outlet context={[error, loading, shopData]} />
         </main>
       </div>
     </>
