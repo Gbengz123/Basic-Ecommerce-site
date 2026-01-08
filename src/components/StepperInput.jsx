29b7abd15a19ab131e82433d77b5d1135580b9d1
Danny <danny@Daniels-MacBook-Air.local>
2026-01-08 23:06:32 +0100

    Handle cart state

--- src/components/StepperInput.jsx
+++ src/components/StepperInput.jsx
@@ -1,34 +1,50 @@
 import React, { useState } from 'react';
 import { PlusIcon, MinusIcon } from 'lucide-react';
 
-function StepperInput() {
-  const [value, setValue] = useState(1);
-
-  function increase() {
-    if (value === 999) return;
-    setValue((value) => value + 1);
+function StepperInput({
+  cartProduct,
+  handleItemAdd,
+  setCartItems,
+  handleRemoveItem,
+}) {
+  function handleIncrease() {
+    handleItemAdd(cartProduct);
   }
 
-  function decrease() {
-    if (value === 0) return;
-    setValue((value) => value - 1);
+  function handleChange(e) {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === cartProduct.id
+          ? {
+              ...item,
+              quantity: Math.max(1, Number(e.target.value) || 1),
+            }
+          : item,
+      ),
+    );
+  }
+
+  function handleDecrease() {
+    handleRemoveItem(cartProduct);
   }
 
   return (
-    <div className="ml-auto flex h-6.75 items-center gap-0.5">
+    <div className="flex h-6.75 items-center gap-0.5">
       <button
-        onClick={increase}
+        onClick={handleIncrease}
         className="d-btn bg-neutral text-neutral-content h-full border-none p-1"
       >
         <PlusIcon height={15} width={20} />
       </button>
       <input
         type="number"
-        value={value}
+        min={'1'}
+        value={cartProduct.quantity}
         className="px-auto no-spinner h-full w-8 rounded border border-zinc-200 text-center text-sm"
+        onChange={(e) => handleChange(e)}
       />
       <button
-        onClick={decrease}
+        onClick={handleDecrease}
         className="d-btn bg-neutral text-neutral-content h-full border-none p-1"
       >
         <MinusIcon height={15} width={20} />
