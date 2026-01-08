e2655d939d45fca339b2828007e9df889a807152
Danny <danny@Daniels-MacBook-Air.local>
2026-01-08 08:04:45 +0100

    Build shop page

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,40 @@
+import { useOutletContext } from 'react-router';
+import spearateCategories from '../utils/separateCategories';
+import ShopCategorySection from './ShopCategorySection';
+
 function Shop() {
-  return <div>Shop</div>;
+  const [error, loading, shopData] = useOutletContext();
+
+  const showSkeleton = loading || error;
+
+  const { womensClothing, mensClothing, jewelery, electronics } =
+    spearateCategories(shopData);
+
+  return (
+    <div className="page-padding flex min-w-[320px] flex-col gap-8">
+      <h1 className="mt-3 text-center text-xl font-bold">Shop</h1>
+      <ShopCategorySection
+        name={"Men's clothing"}
+        products={mensClothing}
+        showSkeleton={showSkeleton}
+      />
+      <ShopCategorySection
+        name={"Women's clothing"}
+        products={womensClothing}
+        showSkeleton={showSkeleton}
+      />
+      <ShopCategorySection
+        name={'Electronics'}
+        products={electronics}
+        showSkeleton={showSkeleton}
+      />
+      <ShopCategorySection
+        name={'jewelery'}
+        products={jewelery}
+        showSkeleton={showSkeleton}
+      />
+    </div>
+  );
 }
 
 export default Shop;
