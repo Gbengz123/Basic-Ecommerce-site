8ee5148a749f22d3f89cd70dfcb1b864aa679acf
Danny <danny@Daniels-MacBook-Air.local>
2026-01-08 15:28:30 +0100

    Edit component

--- src/components/ShopCategorySection.jsx
+++ src/components/ShopCategorySection.jsx
@@ -6,11 +6,14 @@ function ShopCategorySection({ name, products, showSkeleton }) {
     <>
       <section className="">
         <h2 className="mb-3 text-xl font-bold">{name}</h2>
-        <div className="grid grid-cols-[repeat(2,1fr)] gap-3 overflow-x-auto p-1 md:grid-cols-[repeat(4,minmax(200px,260px))] xl:grid-cols-[repeat(auto-fit,minmax(200px,260px))]">
+        <div
+          className={`grid grid-cols-[repeat(2,1fr)] gap-3 overflow-x-auto p-1 md:grid-cols-[repeat(4,minmax(200px,250px))] xl:grid-cols-[repeat(auto-fit,minmax(200px,250px))]`}
+        >
           {!showSkeleton ? (
             <>
               {products.map((product) => (
                 <ProductCard
+                  id={product.id}
                   key={product.id}
                   name={product.title}
                   category={product.category}
