3486edc7f7e8099c00a7e9052b41738b4073b0d9
Danny <danny@Daniels-MacBook-Air.local>
2026-01-09 01:37:17 +0100

    Edit and pass cart item state

--- src/components/ProductsSection.jsx
+++ src/components/ProductsSection.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import ProductCard from './ProductCard';
 import ProductsCarousel from './ProductsCourasel';
 
-function ProductsSection({ showSkeleton, shopData }) {
+function ProductsSection({ showSkeleton, shopData, cartItems }) {
   const products = shopData.filter((_, index) => index % 2 === 0).slice(0, 10);
 
   return (
     <section className="page-padding flex flex-col gap-2 sm:py-4">
       <h2 className="text-xl font-bold">Popular products</h2>
       {!showSkeleton ? (
-        <ProductsCarousel products={products} />
+        <ProductsCarousel products={products} cartItems={cartItems} />
       ) : (
         <div className="flex flex-wrap gap-3">
           {Array.from({ length: 4 }, (_, i) => i).map((index) => {
             return (
-              <div className="xs:w-50 w-40 py-1 sm:w-60">
-                <ProductCard key={index} showSkeleton={showSkeleton} />
+              <div key={index} className="xs:w-50 w-40 py-1 sm:w-60">
+                <ProductCard showSkeleton={showSkeleton} />
               </div>
             );
           })}
