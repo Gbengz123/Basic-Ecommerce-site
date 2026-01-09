fa5427ec0d4f98b85b33db524549e1a36297bd2a
Danny <danny@Daniels-MacBook-Air.local>
2026-01-09 01:38:04 +0100

    Pass cart item states and handlers

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,13 +3,20 @@ import spearateCategories from '../utils/separateCategories';
 import ShopCategorySection from './ShopCategorySection';
 
 function Shop() {
-  const [error, loading, shopData] = useOutletContext();
+  const [
+    error,
+    loading,
+    shopData,
+    cartItems,
+    handleItemAdd,
+    setCartItems,
+    handleRemoveItem,
+  ] = useOutletContext();
 
   const showSkeleton = loading || error;
 
   const { womensClothing, mensClothing, jewelery, electronics } =
     spearateCategories(shopData);
-
   return (
     <div className="page-padding flex min-w-[320px] flex-col gap-8">
       <h1 className="mt-3 text-center text-xl font-bold">Shop</h1>
@@ -17,21 +24,37 @@ function Shop() {
         name={"Men's clothing"}
         products={mensClothing}
         showSkeleton={showSkeleton}
+        cartItems={cartItems}
+        handleItemAdd={handleItemAdd}
+        setCartItems={setCartItems}
+        handleRemoveItem={handleRemoveItem}
       />
       <ShopCategorySection
         name={"Women's clothing"}
         products={womensClothing}
         showSkeleton={showSkeleton}
+        cartItems={cartItems}
+        handleItemAdd={handleItemAdd}
+        setCartItems={setCartItems}
+        handleRemoveItem={handleRemoveItem}
       />
       <ShopCategorySection
         name={'Electronics'}
         products={electronics}
         showSkeleton={showSkeleton}
+        cartItems={cartItems}
+        handleItemAdd={handleItemAdd}
+        setCartItems={setCartItems}
+        handleRemoveItem={handleRemoveItem}
       />
       <ShopCategorySection
         name={'jewelery'}
         products={jewelery}
         showSkeleton={showSkeleton}
+        cartItems={cartItems}
+        handleItemAdd={handleItemAdd}
+        setCartItems={setCartItems}
+        handleRemoveItem={handleRemoveItem}
       />
     </div>
   );
