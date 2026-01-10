4caa9e09c1582ed8178c7d344c4bf2499a85911f
Danny <danny@Daniels-MacBook-Air.local>
2026-01-10 23:36:34 +0100

    Edit message

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -3,13 +3,17 @@ import { CircleAlert } from 'lucide-react';
 
 function Notification({ message }) {
   const isVisible = Boolean(message);
+  const displayMessage =
+    message?.message === 'Failed to fetch'
+      ? 'Unable to load. Please check your internet connection.'
+      : message?.message;
   return (
     <div
       role="alert"
-      className={`alert alert-error xs:w-80 fixed -top-12 left-1/2 z-20 flex w-75 -translate-x-1/2 gap-2 rounded-xl border bg-red-400 p-4 text-red-800 opacity-0 transition-all duration-200 ease-in-out ${isVisible ? `translate-y-[55px] opacity-100` : ``}`}
+      className={`alert alert-error fixed left-1/2 z-20 flex w-fit min-w-70 -translate-x-1/2 gap-2 rounded-xl border bg-red-400 p-4 text-center text-red-800 transition-all duration-200 ease-in-out ${isVisible ? 'top-3 opacity-100' : '-top-12 opacity-0'} `}
     >
       <CircleAlert />
-      <span>Error! {message.message}</span>
+      <span>Error! {displayMessage}</span>
     </div>
   );
 }
