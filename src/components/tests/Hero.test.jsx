855a438208d87303de95631b059816dc18400951
Daniel Olugbenga <danielgebga11@gmail.com>
2026-01-11 23:48:02 +0100

    Add skeleton test

--- src/components/tests/Hero.test.jsx
+++ src/components/tests/Hero.test.jsx
@@ -4,39 +4,54 @@ import { beforeEach, describe, expect, it } from 'vitest';
 import Hero from '../Hero';
 
 describe('Hero', () => {
-  beforeEach(() => {
-    render(
-      <MemoryRouter>
-        <Hero />
-      </MemoryRouter>,
-    );
-  });
-
-  it('shows hero heading', () => {
-    const heroHeading = screen.getByRole('heading', {
-      name: /quality products/i,
+  describe('Hero loaded', () => {
+    beforeEach(() => {
+      render(
+        <MemoryRouter>
+          <Hero showSkeleton={false} />
+        </MemoryRouter>,
+      );
     });
 
-    expect(heroHeading).toBeInTheDocument();
+    it('shows hero heading', () => {
+      const heroHeading = screen.getByRole('heading', {
+        name: /quality products/i,
+      });
+
+      expect(heroHeading).toBeInTheDocument();
+    });
+
+    it('shows hero supporting text', () => {
+      const supportingHeroText = screen.getByText(/everything you need/i);
+      expect(supportingHeroText).toBeInTheDocument();
+    });
+
+    it('shows shop now link', () => {
+      const shopNowLink = screen.getByRole('link', { name: /shop now/i });
+      expect(shopNowLink).toBeInTheDocument();
+    });
+
+    it('shop now link goes to /shop when clicked', () => {
+      const shopNowLink = screen.getByRole('link', { name: /shop now/i });
+      expect(shopNowLink).toHaveAttribute('href', '/shop');
+    });
+
+    it('shows card display', () => {
+      const cardDisplay = screen.getByTestId('card display');
+      expect(cardDisplay).toBeInTheDocument();
+    });
   });
 
-  it('shows hero supporting text', () => {
-    const supportingHeroText = screen.getByText(/everything you need/i);
-    expect(supportingHeroText).toBeInTheDocument();
-  });
+  describe('Hero on load or error', () => {
+    it('shows skeleton on load or error', async () => {
+      render(
+        <MemoryRouter>
+          <Hero showSkeleton={true} />
+        </MemoryRouter>,
+      );
+      const skeleton = await screen.findByTestId('skeleton');
 
-  it('shows shop now link', () => {
-    const shopNowLink = screen.getByRole('link', { name: /shop now/i });
-    expect(shopNowLink).toBeInTheDocument();
-  });
-
-  it('shop now link goes to /shop when clicked', () => {
-    const shopNowLink = screen.getByRole('link', { name: /shop now/i });
-    expect(shopNowLink).toHaveAttribute('href', '/shop');
-  });
-
-  it('shows card display', () => {
-    const cardDisplay = screen.getByTestId('card display');
-    expect(cardDisplay).toBeInTheDocument();
+      expect(skeleton).toBeInTheDocument();
+    });
   });
 });
