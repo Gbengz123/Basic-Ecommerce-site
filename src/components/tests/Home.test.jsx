449a0e5c17bc1970d7641a727d8e7ae69b5528ad
Daniel Olugbenga <danielgebga11@gmail.com>
2026-01-12 00:11:15 +0100

    Edit Home test

--- src/components/tests/Home.test.jsx
+++ src/components/tests/Home.test.jsx
@@ -1,70 +1,28 @@
-import { beforeEach, describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { getByTestId, render, screen } from '@testing-library/react';
+import { MemoryRouter, Outlet } from 'react-router';
+import * as router from 'react-router'; // import all exports
 import Home from '../Home';
 
+// Mock useOutletContext
+vi.spyOn(router, 'useOutletContext').mockReturnValue([
+  false, // error
+  true, // loading
+  [], // shopData
+  [], // cartItems
+]);
+
 describe('Home', () => {
-  beforeEach(() => {
+  it('show home page', () => {
     render(
       <MemoryRouter>
         <Home />
       </MemoryRouter>,
     );
-  });
 
-  it('shows hero section', () => {
-    const heroHeading = screen.getByRole('heading', {
-      name: /quality products/i,
-    });
-    expect(heroHeading).toBeInTheDocument();
-  });
+    screen.debug();
 
-  it('shows popular products heading', () => {
-    const popularProductsHeading = screen.getByRole('heading', {
-      name: /popular products/i,
-    });
-    expect(popularProductsHeading).toBeInTheDocument();
-  });
-
-  it('shows carousel slides', () => {
-    const couraselSlides = screen.getAllByRole('group');
-    expect(couraselSlides.length).toBe(10);
-  });
-
-  it('shows categories heading', () => {
-    const categoriesHeading = screen.getByRole('heading', {
-      name: /categories/i,
-    });
-    expect(categoriesHeading).toBeInTheDocument();
-  });
-
-  describe('shows category links', () => {
-    it('shows mens clothing link', () => {
-      const mensClothingLink = screen.getByRole('link', {
-        name: /mens clothing/i,
-      });
-      expect(mensClothingLink).toBeInTheDocument();
-    });
-
-    it('shows jewelry clothing link', () => {
-      const jewelryLink = screen.getByRole('link', {
-        name: /jewelry/i,
-      });
-      expect(jewelryLink).toBeInTheDocument();
-    });
-
-    it('shows electronics link', () => {
-      const electronicsLink = screen.getByRole('link', {
-        name: /electronics/i,
-      });
-      expect(electronicsLink).toBeInTheDocument();
-    });
-
-    it('shows womens clothing link', () => {
-      const womensClothingLink = screen.getByRole('link', {
-        name: /womens clothing/i,
-      });
-      expect(womensClothingLink).toBeInTheDocument();
-    });
+    const home = screen.getByTestId('home');
+    expect(home).toBeInTheDocument();
   });
 });
