1e425d52542caa397662188be1c5f357e55d5fc1
Daniel Olugbenga <danielgebga11@gmail.com>
2026-01-11 23:48:25 +0100

    Add test id atrribute

--- src/components/ui/skeleton.jsx
+++ src/components/ui/skeleton.jsx
@@ -3,6 +3,7 @@ import { cn } from '@/lib/utils';
 function Skeleton({ className, ...props }) {
   return (
     <div
+      data-testid="skeleton"
       className={cn('animate-pulse rounded-md bg-gray-600/10', className)}
       {...props}
     />
